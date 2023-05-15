256c134b905c6c3cb3a9c31cdb670899040fa0fc
hexaalif <hexaalif2020@gmail.com>
2023-05-15 12:44:51 +0600

    solves

---
+++ Middle_Man.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+
+            if (a[i] > a[j])
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (n % 2 == 0)
+        {
+            if (i == (n - 1) / 2 || i == (((n - 1) / 2) + 1))
+            {
+                printf("%d ", a[i]);
+            }
+        }
+        else
+        {
+
+            if (i == ((n - 1) + 1) / 2)
+            {
+                printf("%d ", a[i]);
+            }
+        }
+    }
+    return 0;
+}
