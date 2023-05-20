ac693f321b62e1b567217e87ee944178665471da
hexaalif <hexaalif2020@gmail.com>
2023-05-20 13:20:27 +0600

    files

--- B_Drawing_X.c
+++ B_Drawing_X.c
@@ -4,11 +4,26 @@ int main()
 {
     int n;
     scanf("%d", &n);
+    int s = n, k = n - 2;
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; i <= n; j++)
+        for (int j = 1; j <= n; j++)
+        {
+            if (j % 2 != 0)
+            {
+
+                printf("\\");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        for (int j = 1; j <= n; j++)
         {
         }
+        printf("\n");
+        // n++;
     }
     return 0;
 }
